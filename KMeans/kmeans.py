06cbd8fe77fba5dcd04ef9e7922d86efc9eed8f0
Boi <2101140015@ms.hanu.edu.vn>
2024-04-07 16:39:20 +0700

    Modify K-Means

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -1,10 +1,8 @@
-import numpy as np
-
-
-class KMeans:
-    def __init__(self):
-        def __init__(self, k=3, num_iter=1000):
+class KMeans():
+    def __init__(self, k = 3, num_iter = 1000):
         """
+            Some initializations, if neccesary
+
             Parameter:
                 k: Number of clusters we are trying to classify
                 num_iter: Number of iterations we are going to loop
@@ -16,17 +14,13 @@ class KMeans:
         self.centers = None
         self.RM = None
 
-    def _cloest_centorids(arr, centers):
-        distance = [np.sqrt(np.sum(np.square(X[j, :] - center))) for
-                    center in centers]
-        minpos = np.argmin(distance)
-        return minpos
-
-    def fit(self, X):
+    def train(self, X):
         """
             Train the given dataset
+
             Parameter:
                 X: Matrix or 2-D array. Input feature matrix.
+
             Return:
                 self: the whole model containing relevant information
         """
@@ -35,28 +29,35 @@ class KMeans:
         centers = []
         RM = np.zeros((r, self.k))
 
-        # initialize centers
-        initials = np.random.choice(r, self.k)
-        for i in initials:
-            centers.append(X[i, :])
-        centers = np.array(centers)
+        """
+            TODO: 1. Modify the following code to randomly choose the initial centers
+        """
+        initials = np.random.choice(r, self.k, replace=False)
+        centers = X[initials]
 
         for i in range(self.num_iter):
             for j in range(r):
-                # calculate cloest centroids
-                minpos = self._cloest_centorids(X[j, :], centers)
+                """
+                    TODO: 2. Modify the following code to update the Relation Matrix
+                """
+                distance = np.linalg.norm(X[j] - centers, axis=1)
+                minpos = np.argmin(distance)
 
                 temp_rm = np.zeros(self.k)
                 temp_rm[minpos] = 1
-                RM[j, :] = temp_rm
-            new_centers = centers.copy()
+                RM[j,:] = temp_rm
+            new_centers = np.zeros_like(centers)
             for l in range(self.k):
-                # find cloest centers
+                """
+                    TODO: 3. Modify the following code to update the centers
+                """
                 row_index = (RM[:, l] == 1).flatten()
                 all_l = X[row_index, :]
-                new_centers[l, :] = np.mean(all_l, axis=0)
-            if np.sum(new_centers - centers) < 0.000000000000000000001:
-                # update centers and RM
+                if len(all_l) > 0:
+                  new_centers[l, :] = np.mean(all_l, axis=0)
+                else:
+                    new_centers[l, :] = centers[l, :]
+            if np.sum(new_centers - centers) < 0.0001:
                 self.centers = new_centers
                 self.RM = RM
                 return self
